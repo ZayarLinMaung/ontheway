0dca2d08387fc140689e0127b24074224e469d43
ZayarLinMaung <163756601+ZayarLinMaung@users.noreply.github.com>
2024-12-28 01:00:35 +0800

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/";
+const API_URL = "http://54.206.54.0/";
 
 // Function to handle user registration
 export const registerUser = async (username, password) => {
